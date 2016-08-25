d9e7b2a211049b0510356cbfe15f667674a30f91
BSTK <bsilva.se@gmail.com>
2016-08-24 22:15:32 -0300

    Commit - Refatoração do javascript para upload de fotos

---
+++ src/main/resources/static/javascript/breeweer/cerveja/cerveja.upload.fotos.js
@@ -0,0 +1,71 @@
+var Breeweer = Breeweer || {};
+
+Breeweer.UploadFoto = (function () {
+	
+	'use-strict';
+	
+	function UploadFoto() {
+		
+		this.inputFoto = $('input[name=foto]');
+		this.inputContentType = $('input[name=contentType]');
+
+		this.fotoCervejaHTMLTemplate = $('#fotoHandlebarsTemplate').html();	
+		this.template = Handlebars.compile(this.fotoCervejaHTMLTemplate);
+	
+		this.uikitUploadDrop = $('#uikitUploadDrop');
+		this.fotoCervejaRegiao = $('.js-foto-cerveja-regiao');
+		
+	};
+	
+	UploadFoto.prototype.iniciar = function () {
+		
+		var settings = {
+				type: 'json',
+				filelimit: 1,
+				allow: '*.(jpg|jpeg|png)',
+				action: this.uikitUploadDrop.data('url-fotos'),
+				complete: onUploadCompleto.bind(this)
+			};
+		
+		UIkit.uploadSelect($('#uikitUploadSelect'), settings);
+		UIkit.uploadDrop(this.uikitUploadDrop, settings);
+		
+	};
+	
+	
+	function onUploadCompleto(resposta) {
+		
+		this.inputFoto.val(resposta.nome);
+		this.inputContentType.val(resposta.contentType);
+		
+		this.uikitUploadDrop.addClass('hidden');
+		
+		var htmlFotoCerveja = this.template({nomeFoto: resposta.nome});
+		this.fotoCervejaRegiao.append(htmlFotoCerveja);
+		
+		$('.js-botao-remove-foto').on('click', onRemoveFoto.bind(this));
+		
+	};
+	
+	
+	function onRemoveFoto() {
+		
+		$('.foto-handlebars-template').remove();
+		this.uikitUploadDrop.removeClass('hidden');
+		this.inputFoto.val('');
+		this.inputContentType.val('');
+		
+	};
+	
+	return UploadFoto;
+	
+}());
+
+(function () {
+	
+	'use-strict';
+	
+	var uploadFotos = new Breeweer.UploadFoto();
+	uploadFotos.iniciar();
+	
+}());
